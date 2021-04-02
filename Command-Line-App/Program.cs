5fe6913be1a1d805f29b0762980355712b4366e4
LenTheDev <lhl7@njit.edu>
2021-04-02 08:44:31 -0400

    Add project files.

---
+++ Command-Line-App/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Command_Line_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = "Len";
+            string date = DateTime.Today.ToShortDateString();
+
+            string str = $"Hello {userName}. Today is {date}.";
+            System.Console.WriteLine(str);
+
+            str = $"{str} How are you today?";
+            System.Console.WriteLine(str);
+        }
+    }
+}
